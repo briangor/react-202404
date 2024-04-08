b2c275462917398e86db28778ac493679db540b3
briangor <briangor13@gmail.com>
2024-04-08 15:39:12 +0300

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders react 202404 link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/React 202404/i);
   expect(linkElement).toBeInTheDocument();
 });
