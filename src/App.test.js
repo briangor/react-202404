04248fbe945722249268d4b4a76f78b11e166817
briangor <briangor13@gmail.com>
2024-04-12 12:03:51 +0300

    update App.test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,8 @@ import App from './App';
 
 test('renders react 202404 link', () => {
   render(<App />);
+  debugger;
   const linkElement = screen.getByText(/React 202404/i);
+  debugger;
   expect(linkElement).toBeInTheDocument();
 });
