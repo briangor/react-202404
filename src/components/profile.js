5f973295959027ae11e6b8f54c8b82d28a917a0e
briangor <briangor13@gmail.com>
2024-04-16 17:32:09 +0300

    add profile

---
+++ src/components/profile.js
@@ -0,0 +1,10 @@
+export default function Profile() {
+    return (
+        <img
+            src="https://i.imgur.com/9WOpRsZ.png"
+            alt="Katherine Johnson"
+            width={200}
+            height={300}
+        />
+    )
+}
