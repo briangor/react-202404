eada3c613a183d03c08051da141993e55cfc9ecf
briangor <briangor13@gmail.com>
2024-04-19 16:54:13 +0300

    wrap app with QueryClientProvider

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './store';
+import { QueryClient, QueryClientProvider } from 'react-query';
+// import { Provider } from 'react-redux';
+// import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}>
       <App />
-    </Provider>
+    </Provider> */}
+    <QueryClientProvider client={new QueryClient()}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
