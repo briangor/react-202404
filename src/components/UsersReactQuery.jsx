b44a34a4c1282b8b0ecf6956f2299d09f1659811
briangor <briangor13@gmail.com>
2024-04-22 14:37:08 +0300

    update useQuery component

--- src/components/UsersReactQuery.jsx
+++ src/components/UsersReactQuery.jsx
@@ -1,5 +1,7 @@
 import { useQuery } from "react-query";
 import axios from "axios";
+import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
 
 export default function UsersReactQuery() {
     const url = 'https://jsonplaceholder.typicode.com/users';
@@ -10,7 +12,23 @@ export default function UsersReactQuery() {
         return data;
     }
 
+    // custom error handling function
+    const handleError = (error, query) => {
+        // do anything with the error here e.g. conditionally render modals with error.message as 
+        // body to imprve the app's ux
+        toast.error(`Something went wrong: ${error.message}`);
+        console.log(`An error occured, ${ error.message }`);
+        return <p>Error ...</p>
+    }
+
     const { isLoading, isError, error, data } = useQuery('users', getUsers);
+
+
+    /* const { isLoading, isError, error, data } = useQuery('users', getUsers, { 
+        //useErrorBoundary: true
+        onError: handleError,
+        useErrorBoundary: error => error.response?.status >= 400
+    }); */
     
     if (isLoading) {
         return <div>Loading...</div>
@@ -20,7 +38,8 @@ export default function UsersReactQuery() {
     
     return (
         <div>
-            <h2>UsersReactQuery</h2>
+            <p><Link to={'/'}>home</Link></p>
+            <h2> <Link to={'/users2'}>UsersReactQuery</Link></h2>
             {data && data.map((user,index) => (
                 <div key={index}>{ user.name }</div>
             ))}
