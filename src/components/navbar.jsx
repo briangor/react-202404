defd0d8ff43c7abd05c8d3b7bc211da6a4fb42d2
briangor <briangor13@gmail.com>
2024-04-29 14:29:57 +0300

    add navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+        <span>React-202404</span>
+        <ul>
+            <li>Home</li>
+            <li>Sign in</li>
+            <li>Get Started</li>
+        </ul>
+    </div>
+  )
+}
