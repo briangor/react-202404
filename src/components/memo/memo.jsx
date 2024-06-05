edaabc41727dec80569dfcf394fc8ad157d49e08
briangor <briangor13@gmail.com>
2024-06-05 17:12:12 +0300

    add memo component

---
+++ src/components/memo/memo.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { createTodos } from "./utils";
+import TodoList from "./TodoList";
+
+const todos = createTodos();
+
+export default function Memo() {
+    const [tab, setTab] = useState('all');
+    const [isDark, setIsDark] = useState(false);
+    return (
+        <>
+        <button onClick={() => setTab('all')}>All</button>
+        <button onClick={() => setTab('active')}>Active</button>
+        <button onClick={() => setTab('completed')}>Completed</button>
+        <br/>
+        <label>
+            <input type="checkbox"
+            checked={isDark}
+            onChange={e => setIsDark(e.target.checked)}
+            /> Red mode
+        </label>
+        <br />
+        <TodoList todos={todos} tab={tab} theme={isDark ? 'red' : 'blue'}/>
+        </>
+    )
+}
