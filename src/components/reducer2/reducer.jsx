a69350daed3198b0b207004c0b1714ca42e6be6b
briangor <briangor13@gmail.com>
2024-06-07 20:37:53 +0300

    add reducer.jsx

---
+++ src/components/reducer2/reducer.jsx
@@ -0,0 +1,14 @@
+import Comparison from './comparison';
+import Form from './form';
+
+export default function Reducer() {
+    return (
+        <div> 
+        <h6>useReducer</h6>
+        <Comparison/>
+        <br />
+        <Form/>
+        </div>
+    )
+}
+
